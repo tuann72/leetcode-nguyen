40d5a1c851efe8f23d93aa65063eea06325e8d91
Tuan Nguyen <tuannguyen08426@gmail.com>
2024-12-29 23:32:12 -0600

    solved 70

--- 70-climbing-stairs.py
+++ 70-climbing-stairs.py
@@ -4,9 +4,22 @@ class Solution:
         given: n steps to reach to top
         return: number of ways to reach to top using combination of steps of 1 or 2
         """
-        ways = 0
+        # memorize patterns that have been solved
+        memo = {1: 1, 2: 2}
 
-        return ways
+        # create function to recursively call
+        def f(n: int):
+            # check if n is in memo
+            if n in memo:
+                # return the item in the memo (we already solved)
+                return memo[n]
+            else:
+                # add new item to meme
+                memo[n] = f(n - 2) + f(n - 1)
+                # return item we just added
+                return memo[n]
+
+        return f(n)
 
 
 solution = Solution()
